3a760e9cf5009f01ecd166b6b5217025e1a881bd
minwoox <songmw725@gmail.com>
2020-10-08 15:04:26 +0900

    Add prometheus exposition

---
+++ src/main/java/armeria/lecture/week3/PrometheusExpositionServer.java
@@ -0,0 +1,28 @@
+package armeria.lecture.week3;
+
+import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.common.metric.MeterIdPrefixFunction;
+import com.linecorp.armeria.common.metric.PrometheusMeterRegistries;
+import com.linecorp.armeria.server.Server;
+import com.linecorp.armeria.server.metric.MetricCollectingService;
+import com.linecorp.armeria.server.metric.PrometheusExpositionService;
+
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+
+public final class PrometheusExpositionServer {
+
+    public static void main(String[] args) {
+        final PrometheusMeterRegistry registry =
+                PrometheusMeterRegistries.defaultRegistry();
+        final Server server = Server.builder()
+                                    .http(8080)
+                                    .service("/", (ctx, req) -> HttpResponse.of(200))
+                                    .decorator(MetricCollectingService.newDecorator(MeterIdPrefixFunction.ofDefault("foo")))
+                                    .meterRegistry(registry)
+                                    .service("/prometheus", new PrometheusExpositionService(
+                                            registry.getPrometheusRegistry()))
+                                    .build();
+        server.start().join();
+
+    }
+}
